f159454e9d09383ca2b37857bace32c36bcdb7fc
Rosemary Shanley <rshanley@thisisfusion.com>
2015-10-20 14:51:30 -0500

    dealing with merge issues

--- platforms/ios/www/js/custom_camera.js
+++ platforms/ios/www/js/custom_camera.js
@@ -1,9 +1,5 @@
 var CustomCamera = {
-<<<<<<< HEAD
-    startVideoCapture: function(success, failure){
-=======
 	getVideo: function(success, failure){
->>>>>>> 728006c0e8113181a5a18aa5bfeac64c8cd5c9da
 		cordova.exec(success, failure, "CustomCamera", "openCamera", []);
 	}
 };
